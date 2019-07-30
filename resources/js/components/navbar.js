27a1b7c33d6fe97b8585da122724f87e34e00d34
DreddBG <ninjakiviWTF@gmail.com>
2019-07-30 16:57:02 +0300

    Public /vendor

--- resources/js/components/navbar.js
+++ resources/js/components/navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom'
+import Dashboard from './dashboard'
  
 export default class navbar extends Component {
     render() {
@@ -9,6 +10,7 @@ export default class navbar extends Component {
                 <div className="container">
                     <Link to='/'>Home</Link>
                     <Link to='/create'>Create</Link>
+                    <a href="/" component={Dashboard} >NEW ONE</a>
                 </div>
             </nav>
        );
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>This is React View</h1>
     <div id="reactapp"></div>
     <script src="{{ mix('js/app.js') }}"></script>
 
